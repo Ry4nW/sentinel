05dd9adebf4ce49b4df7e65f2d730a04c1d4faab
Ry4nW <ryanbcwang@gmail.com>
2025-07-01 15:36:12 -0700

    test: test xss detection

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -14,3 +14,19 @@ def crawler():
 
 
 def make_soup(html):
+    return BeautifulSoup(html, 'html.parser')
+
+
+class TestGetFormDetails:
+    def test_parses_action_and_method(self, crawler):
+        soup = make_soup('<form action="/search" method="post"><input type="text" name="q"></form>')
+        form = soup.find('form')
+        details = crawler.get_form_details(form)
+        assert details['action'] == '/search'
+        assert details['method'] == 'post'
+
+    def test_defaults_method_to_get(self, crawler):
+        soup = make_soup('<form action="/go"><input type="text" name="q"></form>')
+        form = soup.find('form')
+        details = crawler.get_form_details(form)
+        assert details['method'] == 'get'
